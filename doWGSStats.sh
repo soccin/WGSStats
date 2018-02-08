95e237853bfdae9fb091a4114f8f2840ae946d40
Nicholas D. Socci <soccin@mskcc.org>
2018-02-07 23:08:05 -0500

    Added GENOME arg

--- doWGSStats.sh
+++ doWGSStats.sh
@@ -4,12 +4,29 @@
 
 SDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ "$#" != "1" ]; then
+if [ "$#" -lt "1" ]; then
     echo
-    echo "   usage: WGSStats/doWGSStats.sh PROJECTDIR"
+    echo "   usage: WGSStats/doWGSStats.sh [-g GENOME] PROJECTDIR"
+    echo
+    echo "      human_b37"
+    echo "      mouse_mm10"
     echo
     exit
 fi
+
+GENOME=human_b37
+
+while getopts "g:" opt; do
+    case "$opt" in
+        g)
+            GENOME=$OPTARG
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+echo GENOME=$GENOME
 
 PROJECTDIR=$1
 project=$(echo $PROJECTDIR | perl -ne 'm|/(Project_[^/]*)|;print $1')
@@ -29,5 +46,5 @@ mappingSheet=${project/Project/Proj}_sample_mapping.txt
 getMappingSheet.sh sampleDIRs_$$ >$mappingSheet
 sleep 15
 
-$SDIR/PEMapper/runPEMapperMultiDirectories.sh human_b37 $mappingSheet
+$SDIR/PEMapper/runPEMapperMultiDirectories.sh $GENOME $mappingSheet
 
