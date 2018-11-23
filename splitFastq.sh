5b71f62f999f8d8ea3bf70027a01a106d7e6b4b1
Nicholas D. Socci <soccin@mskcc.org>
2018-11-23 16:34:37 -0500

    Add runid to output folder path
    Need this for samples that were run on multiple machine runs

--- splitFastq.sh
+++ splitFastq.sh
@@ -10,11 +10,12 @@ read=$(basename $file | perl -ne 'm/_(R[12])_/;print "$1"')
 base=$(dirname $file)
 samp=$(basename $base | sed 's/_IGO.*//')
 project=$(echo $file | perl -ne 'm|/(Project_.*?)/|;print $1')
+runid=$(basename $(echo $file | perl -pe 's|/Project_.*||'))
 
 echo $file
 echo $read, $samp
 
-ODIR=FASTQ/$project/$samp
+ODIR=FASTQ/$project/$runid/$samp
 mkdir -p $ODIR
 zcat $file | split -a 3 -d -l 16000000 - $ODIR/${samp/Sample_/}_${read}_
 
