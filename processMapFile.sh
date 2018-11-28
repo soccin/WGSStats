5369fd13461d6d6620b409b911404a3298ff0475
Nicholas D. Socci <soccin@mskcc.org>
2018-11-28 17:29:51 -0500

    Fix script path

--- processMapFile.sh
+++ processMapFile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
 MAP=$1
 
 cat $MAP | cut -f 4 | sed 's/.Sample_.*//' | sort | uniq \
-    | xargs bsub -app anyOS -R "select[type==CentOS7]" -W 96:00 -n 1 -J CTRL -o LSF.CTRL/ ./WGSStats/doWGSStats.sh
+    | xargs bsub -app anyOS -R "select[type==CentOS7]" -W 96:00 -n 1 -J CTRL -o LSF.CTRL/ $SDIR/doWGSStats.sh
